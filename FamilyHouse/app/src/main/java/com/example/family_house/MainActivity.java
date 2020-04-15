60a9902b66e33e9302cad041aa799a282ae9a182
Riellytruffa <54604958+Riellytruffa@users.noreply.github.com>
2020-04-15 18:22:49 -0400

    merged all pages

--- FamilyHouse/app/src/main/java/com/example/family_house/MainActivity.java
+++ FamilyHouse/app/src/main/java/com/example/family_house/MainActivity.java
@@ -15,10 +15,41 @@ public class MainActivity extends AppCompatActivity {
     public Button button3;
     public Button button6;
     public Button button7;
+    public Button button8;
+    public Button button;
+    public Button button4;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LinensActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FAQActivity.class);
+                startActivity(intent);
+            }
+        });
+
         button7 = (Button) findViewById(R.id.button7);
         button7.setOnClickListener(new View.OnClickListener() {
             @Override
